deb8059426e01a2cba403ab43f6805c14426e337
Léo JULLEROT <jullerot.leo.BTS.SIO@gmail.com>
2017-11-14 10:21:28 +0100

    Ajout btn tab
    Commit du déséspoir... Ajout des boutons au tableau de participants

--- EOLE/src/LancementRegate.java
+++ EOLE/src/LancementRegate.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,14 +23,16 @@ public class LancementRegate extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JTable tableParticipants;
-	private JPanel panelSelRegate, panelChrono, panelInfoRegate;
-	private JLabel lblSelRegate, lblNomRegate, lblDate, lblLieuDepart, lblLieuArrivee, lblDistance, label;
+	private JPanel panelSelRegate, panelChrono, panelInfoRegate, panelTableParticipant;
+	private JLabel lblSelRegate, lblNomRegate, lblDate, lblLieuDepart, lblLieuArrivee, lblDistance, lblChrono;
 	private JTextField tfNomRegate, tfLieuDepart, tfLieuArrivee, tfDistance;
 	private JComboBox<String> cboSelRegate;
 	private JDateChooser cboDate;
 	private DTimer chrono;
 	private Window window;
 	private String[] listeRegate;
+	private JButton btnFin, btnReinit, btnDepart;
+	private JScrollPane scrollPane;
 
 	public LancementRegate(Window window, String[] listeRegate) {
 		this.window = window;
@@ -38,147 +41,151 @@ public class LancementRegate extends JFrame {
 	
 	public void creationPanelSelRegate() {
 		
-		panelSelRegate = new JPanel();
-		panelSelRegate.setBounds(10, 11, 764, 57);
-		window.add(panelSelRegate);
-		panelSelRegate.setLayout(null);
+		this.panelSelRegate = new JPanel();
+		this.panelSelRegate.setBounds(10, 11, 764, 57);
+		this.window.add(panelSelRegate);
+		this.panelSelRegate.setLayout(null);
 		
-		lblSelRegate = new JLabel("Selectionner la régate à lancer : ");
-		lblSelRegate.setBounds(217, 14, 156, 14);
-		panelSelRegate.add(lblSelRegate);
+		this.lblSelRegate = new JLabel("Selectionner la régate à lancer : ");
+		this.lblSelRegate.setBounds(217, 14, 156, 14);
+		this.panelSelRegate.add(lblSelRegate);
 		
-		cboSelRegate = new JComboBox<String>(listeRegate);
-		cboSelRegate.setBounds(383, 11, 161, 20);
-		panelSelRegate.add(cboSelRegate);
+		this.cboSelRegate = new JComboBox<String>(listeRegate);
+		this.cboSelRegate.setBounds(383, 11, 161, 20);
+		this.panelSelRegate.add(cboSelRegate);
 	}
 	
 	public void creationPanelInfoRegate() {
-		panelInfoRegate = new JPanel();
-		panelInfoRegate.setBorder(new TitledBorder(null, "R\u00E9gate", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelInfoRegate.setBounds(10, 79, 370, 234);
-		window.add(panelInfoRegate);
+		this.panelInfoRegate = new JPanel();
+		this.panelInfoRegate.setBorder(new TitledBorder(null, "R\u00E9gate", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		this.panelInfoRegate.setBounds(10, 79, 370, 234);
+		this.window.add(panelInfoRegate);
 		
-		lblNomRegate = new JLabel("Nom de la régate : ");
-		lblNomRegate.setBounds(29, 21, 96, 43);
-		lblDate = new JLabel("Date : ");
-		lblDate.setBounds(87, 52, 38, 43);
-		lblLieuDepart = new JLabel("Lieu de départ : ");
-		lblLieuDepart.setBounds(46, 92, 79, 43);
-		lblLieuArrivee = new JLabel("Lieu d'arrivée : ");
-		lblLieuArrivee.setBounds(51, 130, 74, 43);
-		lblDistance = new JLabel("Distance : ");
-		lblDistance.setBounds(68, 165, 57, 43);
-		tfNomRegate = new JTextField(15); 
-		tfNomRegate.setBounds(135, 29, 185, 26);
-		tfLieuDepart = new JTextField(15);
-		tfLieuDepart.setBounds(135, 100, 185, 26);
-		tfLieuArrivee = new JTextField(15);
-		tfLieuArrivee.setBounds(135, 138, 185, 26);
-		tfDistance = new JTextField(15);
-		tfDistance.setBounds(135, 173, 185, 26);
-		cboDate = new JDateChooser();
-		cboDate.setBounds(135, 64, 118, 26);
-		panelInfoRegate.setLayout(null);
+		this.lblNomRegate = new JLabel("Nom de la régate : ");
+		this.lblNomRegate.setBounds(29, 21, 96, 43);
+		this.lblDate = new JLabel("Date : ");
+		this.lblDate.setBounds(87, 52, 38, 43);
+		this.lblLieuDepart = new JLabel("Lieu de départ : ");
+		this.lblLieuDepart.setBounds(46, 92, 79, 43);
+		this.lblLieuArrivee = new JLabel("Lieu d'arrivée : ");
+		this.lblLieuArrivee.setBounds(51, 130, 74, 43);
+		this.lblDistance = new JLabel("Distance : ");
+		this.lblDistance.setBounds(68, 165, 57, 43);
+		this.tfNomRegate = new JTextField(15); 
+		this.tfNomRegate.setEditable(false);
+		this.tfNomRegate.setBounds(135, 29, 185, 26);
+		this.tfLieuDepart = new JTextField(15);
+		this.tfLieuDepart.setEditable(false);
+		this.tfLieuDepart.setBounds(135, 100, 185, 26);
+		this.tfLieuArrivee = new JTextField(15);
+		this.tfLieuArrivee.setEditable(false);
+		this.tfLieuArrivee.setBounds(135, 138, 185, 26);
+		this.tfDistance = new JTextField(15);
+		this.tfDistance.setEditable(false);
+		this.tfDistance.setBounds(135, 173, 185, 26);
+		this.cboDate = new JDateChooser();
+		this.cboDate.setBounds(135, 64, 118, 26);
+		this.panelInfoRegate.setLayout(null);
 		
-		panelInfoRegate.add(lblNomRegate);
-		panelInfoRegate.add(tfNomRegate);
-		panelInfoRegate.add(lblDate);
-		panelInfoRegate.add(cboDate);
-		panelInfoRegate.add(lblLieuDepart);
-		panelInfoRegate.add(tfLieuDepart);
-		panelInfoRegate.add(lblLieuArrivee);
-		panelInfoRegate.add(tfLieuArrivee);
-		panelInfoRegate.add(lblDistance);
-		panelInfoRegate.add(tfDistance);
+		this.panelInfoRegate.add(lblNomRegate);
+		this.panelInfoRegate.add(tfNomRegate);
+		this.panelInfoRegate.add(lblDate);
+		this.panelInfoRegate.add(cboDate);
+		this.panelInfoRegate.add(lblLieuDepart);
+		this.panelInfoRegate.add(tfLieuDepart);
+		this.panelInfoRegate.add(lblLieuArrivee);
+		this.panelInfoRegate.add(tfLieuArrivee);
+		this.panelInfoRegate.add(lblDistance);
+		this.panelInfoRegate.add(tfDistance);
 	}
 	
 	public void creationPanelChrono() {
-		chrono = new DTimer(this);
+		this.chrono = new DTimer(this);
 		
-		panelChrono = new JPanel();
-		panelChrono.setBorder(new TitledBorder(null, "Chronom\u00E8tre", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelChrono.setBounds(10, 334, 370, 157);
-		window.add(panelChrono);
-		panelChrono.setLayout(null);
+		this.panelChrono = new JPanel();
+		this.panelChrono.setBorder(new TitledBorder(null, "Chronom\u00E8tre", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		this.panelChrono.setBounds(10, 334, 370, 157);
+		this.window.add(panelChrono);
+		this.panelChrono.setLayout(null);
 		
 
 		
-		JButton btnFin = new JButton("FIN");
-		btnFin.addActionListener(new ActionListener() {
+		this.btnFin = new JButton("FIN");
+		this.btnFin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				chrono.stopDTimer();
 			}
 		});
-		btnFin.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnFin.setBounds(272, 42, 88, 59);
-		panelChrono.add(btnFin);
+		this.btnFin.setFont(new Font("Tahoma", Font.BOLD, 12));
+		this.btnFin.setBounds(272, 42, 88, 59);
+		this.panelChrono.add(btnFin);
 		
-		JButton btnNewButton_2 = new JButton("Reinitialiser");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		this.btnReinit = new JButton("Reinitialiser");
+		this.btnReinit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				chrono.reinitDTimer();
-				label.setText("00:00:00");
+				lblChrono.setText("00:00:00");
 			}
 		});
-		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnNewButton_2.setBounds(171, 112, 189, 23);
-		panelChrono.add(btnNewButton_2);
-		btnNewButton_2.setToolTipText("");
+		this.btnReinit.setFont(new Font("Tahoma", Font.BOLD, 12));
+		this.btnReinit.setBounds(171, 112, 189, 23);
+		this.panelChrono.add(btnReinit);
+		this.btnReinit.setToolTipText("");
 		
-		label = new JLabel("00:00:00");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 37));
-		label.setBounds(10, 42, 171, 76);
-		panelChrono.add(label);	
+		this.lblChrono = new JLabel("00:00:00");
+		this.lblChrono.setFont(new Font("Tahoma", Font.PLAIN, 37));
+		this.lblChrono.setBounds(10, 42, 171, 76);
+		this.panelChrono.add(lblChrono);	
 		
-		JButton btnNewButton_1 = new JButton("DEPART");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnNewButton_1.addActionListener(new ActionListener() {
+		this.btnDepart = new JButton("DEPART");
+		this.btnDepart.setFont(new Font("Tahoma", Font.BOLD, 12));
+		this.btnDepart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				chrono.startDTimer();
 			}
 		});
-		btnNewButton_1.setBounds(174, 42, 88, 59);
-		panelChrono.add(btnNewButton_1);
+		this.btnDepart.setBounds(174, 42, 88, 59);
+		this.panelChrono.add(btnDepart);
 	}
 	
 	public void creationPanelTableau() {
 		
-		JPanel panelTableParticipant = new JPanel();
-		panelTableParticipant.setBorder(new TitledBorder(null, "Participants", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelTableParticipant.setBounds(404, 79, 370, 412);
-		window.add(panelTableParticipant);
-		panelTableParticipant.setLayout(null);
+		this.panelTableParticipant = new JPanel();
+		this.panelTableParticipant.setBorder(new TitledBorder(null, "Participants", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		this.panelTableParticipant.setBounds(404, 79, 370, 412);
+		this.window.add(panelTableParticipant);
+		this.panelTableParticipant.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 29, 350, 357);
-		panelTableParticipant.add(scrollPane);
+		scrollPane = new JScrollPane();
+		this.scrollPane.setBounds(10, 29, 350, 357);
+		this.panelTableParticipant.add(scrollPane);
 		
-		tableParticipants = new JTable(20,5);
-		tableParticipants.setBorder(new LineBorder(new Color(0, 0, 0)));
-		scrollPane.setViewportView(tableParticipants);
-		tableParticipants.setFillsViewportHeight(true);
-		tableParticipants.setModel(new DefaultTableModel(
+		this.tableParticipants = new JTable(20,5);
+		this.tableParticipants.setBorder(new LineBorder(new Color(0, 0, 0)));
+		this.scrollPane.setViewportView(tableParticipants);
+		this.tableParticipants.setFillsViewportHeight(true);
+		this.tableParticipants.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
+				{null, "Arrivée", "Abandon", "00:00:00", "00:00:00"},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				{null, "Arrivée", "Abandon", null, null},
+				
 			},
 			new String[] {"Participant", "Arriv\u00E9e", "Abandon", "Temp r\u00E9el", "Temp compens\u00E9"}
 		) {
@@ -192,15 +199,22 @@ public class LancementRegate extends JFrame {
 			}
 		});
 		
-		tableParticipants.getColumnModel().getColumn(1).setPreferredWidth(50);
-		tableParticipants.getColumnModel().getColumn(2).setPreferredWidth(50);
-		tableParticipants.getColumnModel().getColumn(3).setPreferredWidth(70);
-		tableParticipants.getColumnModel().getColumn(4).setPreferredWidth(70);
+		tableParticipants.getColumn("Arriv\u00E9e").setCellRenderer(new ButtonRenderer());
+		tableParticipants.getColumn("Arriv\u00E9e").setCellEditor(
+		        new ButtonEditor(new JCheckBox()));
+		tableParticipants.getColumn("Abandon").setCellRenderer(new ButtonRenderer());
+		tableParticipants.getColumn("Abandon").setCellEditor(
+		        new ButtonEditor(new JCheckBox()));
+		
+		this.tableParticipants.getColumnModel().getColumn(1).setPreferredWidth(50);
+		this.tableParticipants.getColumnModel().getColumn(2).setPreferredWidth(50);
+		this.tableParticipants.getColumnModel().getColumn(3).setPreferredWidth(70);
+		this.tableParticipants.getColumnModel().getColumn(4).setPreferredWidth(70);
 	}
 	
 	public void setChrono(int timeCount) {
 		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
-		this.label.setText(df.format(timeCount - 3.6 * Math.pow(10,6)));
+		this.lblChrono.setText(df.format(timeCount - 3.6 * Math.pow(10,6)));
 		
 	}
 }
