3c200017308b9d39511ba94707e6b0bac7f8dad2
Léo JULLEROT <jullerot.leo.BTS.SIO@gmail.com>
2017-11-20 12:21:13 +0100

    Mise au propre classe AjoutRegate

--- EOLE/src/Accueil.java
+++ EOLE/src/Accueil.java
@@ -1,30 +1,21 @@
 import java.awt.Font;
+import java.awt.Window;
 
 import javax.swing.*;
 
 public class Accueil extends JFrame{
 	private Window window;
-	private JLabel JImageBateau= new JLabel(new ImageIcon("bateau2.gif"));
-	private JPanel panImage;
-	private JLabel lblTitle;
+	private JLabel JImageBateau = new JLabel(new ImageIcon("bateau2.gif"));
+	private JPanel cpImage = new JPanel();
 	
 	public Accueil(Window window){
 		this.window= window;
 	}
 	
 	public void createAccueil(){
-		panImage = new JPanel();
-		panImage.add(JImageBateau);
-		panImage.setLayout(null);
-		this.window.add(panImage);
-		
-		lblTitle = new JLabel("AJOUT PARTICIPANT");
-		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 40));
-		lblTitle.setBounds(10, 14, 744, 32);
-		this.panImage.add(lblTitle);
-		
-	
+		this.cpImage.setBounds(0, 0, window.getWidth(), window.getHeight());
+		this.cpImage.add(JImageBateau);
+		this.window.add(cpImage);
 	}
 
 }
